5eb6322622167a18db2e55d2c264a7be9cc7ae21
aditya2299 <60549739+aditya2299@users.noreply.github.com>
2022-05-02 13:00:35 +0530

    loans sb creation

---
+++ loans/src/main/java/com/example/loans/controller/LoansController.java
@@ -0,0 +1,31 @@
+package com.example.loans.controller;
+
+import com.example.loans.model.Customer;
+import com.example.loans.model.Loans;
+import com.example.loans.repository.LoansRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoansController {
+
+    @Autowired
+    private LoansRepository loansRepository;
+
+    @PostMapping("/myLoans")
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+        System.out.println("inside");
+        List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+        if (loans != null) {
+            return loans;
+        } else {
+            return null;
+        }
+
+    }
+
+}
